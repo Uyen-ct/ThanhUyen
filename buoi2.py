50ade9b0c1fbe25d2701bcc52278bda091f12066
Uyen-ct <truongthithanhuyena534@gmail.com>
2024-08-18 11:13:16 +0700

    Add files via upload

---
+++ buoi2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 06:53:02 2024
+
+@author: TruongThiThanhUyen
+"""
+distance=float(input("Nhập độ dài đoạn đường đến trường (m):"))
+if distance>1200:
+        print("Đường đến trường quá xa.")
+        
